96afdf96c4637dfd7635b386d54b712aba071746
Nguyễn Trịnh Thành <90306525+Thanh10101@users.noreply.github.com>
2024-05-23 12:30:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// Lấy phần tử có class 'my-class'
 var element = document.querySelector('.entry-content');
-
-// Lấy nội dung văn bản của phần tử
 var text = element.textContent || element.innerText;
-
-// Tách văn bản thành các từ bằng cách sử dụng split với dấu cách
 var words = text.trim().split(/\s+/);
 
-// Đếm số từ
 var wordCount = words.length;
-
-// In ra kết quả
 console.log("Số từ trong phần tử là: " + wordCount);
